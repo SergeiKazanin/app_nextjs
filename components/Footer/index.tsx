b399880ee2ec961445e33873275f0cc4749f5fad
SergeiKazanin <serega7667@gmail.com>
2023-09-08 15:31:33 +0700

    start project

---
+++ components/Footer/index.tsx
@@ -0,0 +1,5 @@
+import style from "./index.module.scss";
+
+export const Footer = () => {
+  return <div className={style.footer}>Footer</div>;
+};
---
+++ components/Header/index.tsx
@@ -0,0 +1,5 @@
+import style from "./index.module.scss";
+
+export const Header = () => {
+  return <header className={style.header}>Header</header>;
+};
